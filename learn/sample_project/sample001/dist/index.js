6be9a5cca9cd30d432f7307a0f16aa5c0ecedb15
kazumatu981 <kazumatu981@gmail.com>
2026-07-24 19:13:08 +0900

    アプリ開発完了

--- learn/sample_project/sample001/dist/index.js
+++ learn/sample_project/sample001/dist/index.js
@@ -1,8 +1,4 @@
-import { Counter } from './lib/counter.js';
-import { Controller } from './lib/controller.js';
-
-const counter = new Counter('counter');
-const controller = new Controller('increment', 'decrement');
+import { App } from './app.js';
 
 // ドキュメントをすべて読み込んだら onPageLoad関数を呼び出す
 document.addEventListener('DOMContentLoaded', onPageLoad);
@@ -13,36 +9,10 @@ document.addEventListener('DOMContentLoaded', onPageLoad);
 function onPageLoad() {
     console.log('Page loaded');
 
-    // カウンタの値の変化を監視する
-    counter.on('change', onCounterChange);
-
-    // コントローラのイベントを登録する
-    controller.on('click_increment', onIncrement);
-    controller.on('click_decrement', onDecrement);
-
-    // 初期状態を設定する
-    counter.value = 1;
-}
-
-function onCounterChange() {
-    // もし、上限に達したら、インクリメントボタンを無効化する
-    if (counter.value >= counter.maximum) {
-        controller.enableIncrement = false;
-    } else {
-        controller.enableIncrement = true;
-    }
-    // もし、下限に達したら、デクリメントボタンを無効化する
-    if (counter.value <= counter.minimum) {
-        controller.enableDecrement = false;
-    } else {
-        controller.enableDecrement = true;
-    }
-}
-
-function onIncrement() {
-    counter.increment();
-}
-
-function onDecrement() {
-    counter.decrement();
+    // App クラスのインスタンスを作成する
+    const app = new App({
+        counter: 'counter',
+        increment: 'increment',
+        decrement: 'decrement',
+    });
 }
