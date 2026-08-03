ac15b05e2233fca36f64a4ffd7b1ea65e3c269c1
kazumatu981 <kazumatu981@gmail.com>
2026-08-04 00:48:02 +0900

    just backup.

--- learn/learning_project/dice/dist/lib/dice.js
+++ learn/learning_project/dice/dist/lib/dice.js
@@ -54,8 +54,7 @@ export class Dice extends EventHandler {
     _element = null;
     _value = 1;
     _state = ROLLING_STATE.stopped;
-    _untilStop = UNTIL_STOP_DEFAULT;
-    _stoppingCounter = 0;
+    _stoppingCounter = UNTIL_STOP_DEFAULT;
 
     //#region constructor
     /**
@@ -79,10 +78,17 @@ export class Dice extends EventHandler {
     //#endregion
 
     //#region public properties
+    /**
+     * さいころの目を取得する。
+     * @returns {number} さいころの目
+     */
     get value() {
         return this._value;
     }
-
+    /**
+     * さいころの目を設定する。
+     * @param {number} newValue さいころの目
+     */
     set value(newValue) {
         // 値の型と範囲をチェックする
         __assertIsNumber(newValue);
@@ -100,17 +106,23 @@ export class Dice extends EventHandler {
     get state() {
         return this._state;
     }
-
     //#endregion
 
     //#region public methods
+    /**
+     * さいころを転がす
+     * すでに転がっている場合は何もしない
+     */
     start() {
         if (this.state !== ROLLING_STATE.stopped) {
             return;
         }
         this._unsafeState = ROLLING_STATE.started;
     }
-
+    /**
+     * さいころを止める
+     * すでに止まっている場合は何もしない
+     */
     stop() {
         if (this.state !== ROLLING_STATE.started) {
             return;
@@ -120,20 +132,21 @@ export class Dice extends EventHandler {
     //#endregion
 
     //#region private members
+
+    //#region private properties
+    /**
+     * さいころの状態に応じたループの実行間隔を取得する
+     */
     get _interval() {
         return this.state === ROLLING_STATE.started
             ? ROLLING_INTERVAL
             : STOPPING_INTERVAL;
     }
-
-    get _shouldBreakLoop() {
-        return (
-            this.state === ROLLING_STATE.stopped ||
-            (this.state === ROLLING_STATE.stopping &&
-                this._untilStop < this._stoppingCounter)
-        );
-    }
-
+    /**
+     * さいころの状態を設定する
+     * 状態が変化した場合は、対応するイベントを発火する
+     * @param {number} value さいころの状態
+     */
     set _unsafeState(value) {
         if (this._state !== value) {
             this._state = value;
@@ -141,22 +154,36 @@ export class Dice extends EventHandler {
         }
     }
 
+    /**
+     * さいころの目を設定する
+     * 目が変化した場合は、対応するイベントを発火する
+     * @param {number} value さいころの目
+     */
     set _unsafeValue(value) {
         this._value = value;
         this.fire(EVENT_NAME_CHANGED, this);
     }
+    //#endregion
 
+    /**
+     * さいころが転がり始めたときに発火するイベントの処理
+     */
     _onStarted() {
         this._initLoop();
         this._runLoop();
     }
-
+    /**
+     * さいころの目が変化したときに発火するイベントの処理
+     */
     _onChanged() {
         if (this._element) {
             this._element.textContent = this.value.toString();
         }
         if (this.state === ROLLING_STATE.stopping) {
-            this._stoppingCounter++;
+            this._stoppingCounter--;
+            if (this._stoppingCounter <= 0) {
+                this._unsafeState = ROLLING_STATE.stopped;
+            }
         }
     }
 
@@ -165,19 +192,17 @@ export class Dice extends EventHandler {
      */
     _initLoop() {
         // UNTIL_STOP_DEFAULTを中心に±UNTIL_STOP_AROUNDの範囲で
-        this._untilStop = getRandomValue(
+        this._stoppingCounter = getRandomValue(
             UNTIL_STOP_DEFAULT - UNTIL_STOP_AROUND,
             UNTIL_STOP_DEFAULT + UNTIL_STOP_AROUND
         );
-        this._stoppingCounter = 0;
     }
     /**
-     *
+     * さいころ目変更メインループ
      */
     _runLoop() {
         // さいころを止めるべき場合は何もせずにそこで終わる
-        if (this._shouldBreakLoop) {
-            this._unsafeState = ROLLING_STATE.stopped;
+        if (this.state === ROLLING_STATE.stopped) {
             return;
         }
 
@@ -187,7 +212,9 @@ export class Dice extends EventHandler {
         // 実行間隔後に再起呼び出しをする
         setTimeout(this._runLoop.bind(this), this._interval);
     }
-
+    /**
+     * さいころの目を次の目にする
+     */
     _roll() {
         this._unsafeValue = getRandomValue(1, 6, this.value);
     }
@@ -195,7 +222,8 @@ export class Dice extends EventHandler {
 }
 
 /**
- * 最小値と最大値の間の値の乱数を取得する
+ * 最小値と最大値の間の値の乱数を取得する。
+ * ひとつ前の値と同じ値が出ないようにする。
  * @param {number} min 最小の値
  * @param {number} max 最大の値
  * @param {number || undefined} prev 一つ前の値
--- learn/learning_project/dice/dist/lib/start-stop-button.js
+++ learn/learning_project/dice/dist/lib/start-stop-button.js
@@ -5,9 +5,9 @@ import {
 } from './assert.js';
 import { EventHandler } from './event-handler.js';
 
-const BUTTON_TYPE_START = 'start';
-const BUTTON_TYPE_STOPPING = 'stopping';
-const BUTTON_TYPE_STOP = 'stop';
+export const BUTTON_TYPE_START = 'start';
+export const BUTTON_TYPE_STOPPING = 'stopping';
+export const BUTTON_TYPE_STOP = 'stop';
 
 const BUTTON_FACE = {
     [BUTTON_TYPE_START]: 'Start',
@@ -28,39 +28,74 @@ export class StartStopButton extends EventHandler {
     _buttonType = BUTTON_TYPE_START;
     _element = null;
 
+    //#region constructor
     constructor(elementId) {
         super();
         __assertIsString(elementId);
         this._element = __safeGetElementById(elementId);
         this._registerEvent();
     }
+    //#endregion
 
+    //#region public properties
+    /**
+     * ボタンの種類を取得する。
+     * @returns {string} ボタンの種類
+     * @see BUTTON_TYPE_START
+     * @see BUTTON_TYPE_STOPPING
+     * @see BUTTON_TYPE_STOP
+     */
     get buttonType() {
         return this._buttonType;
     }
 
+    /**
+     * ボタンの種類を設定する。
+     * @param {string} value ボタンの種類
+     * @see BUTTON_TYPE_START
+     * @see BUTTON_TYPE_STOPPING
+     * @see BUTTON_TYPE_STOP
+     */
     set buttonType(value) {
         // TODO ボタンの種類が正しいかどうかを検証して、タイプを設定する
         // その後、ボタンの見た目を更新するために _updateLookAndFeel() を呼び出す
     }
+    //#endregion
 
+    //#region private methods
+    /**
+     * DOM要素のクリックイベントを登録する
+     */
     _registerEvent() {
         // TODO DOM要素のクリックイベントを登録する
-        // ボタンをクリックしたら。。。。。
-        // - ボタンの種類が start の場合は、click_start イベントを発火する
-        // - ボタンの種類が stop の場合は、click_stop イベントを発火する
-        // - ボタンの種類が stopping の場合は、何もしない
+        this._element.addEventListener('click', () => {
+            // TODO ボタンをクリックしたときの処理を実装する
+            // ボタンをクリックしたら。。。。。
+            // - ボタンの種類が start の場合は、click_start イベントを発火する
+            // - ボタンの種類が stop の場合は、click_stop イベントを発火する
+            // - ボタンの種類が stopping の場合は、何もしない
+        });
     }
 
+    /**
+     * ボタンの見た目を更新する
+     */
     _updateLookAndFeel() {
         this._setFace();
         this._setClass();
     }
+    /**
+     * DOM要素のテキストをボタンの種類に応じて変更する
+     */
     _setFace() {
         // TODO DOM要素のテキストをボタンの種類に応じて変更する
     }
 
+    /**
+     * DOM要素のクラスをボタンの種類に応じて変更する
+     */
     _setClass() {
         // TODO DOM要素のクラスをボタンの種類に応じて変更する
     }
+    //#endregion
 }
